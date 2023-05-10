072f5d19f8dd60cb1a2ee1896fdc1e34c2627ed6
Your Name <yourgmail address>
2023-05-10 14:17:42 +0300

    cancel refactoring for now && add delete message logic

--- src/Components/DeleteModal.jsx
+++ src/Components/DeleteModal.jsx
@@ -1,34 +1,43 @@
 import { Modal, Button } from 'antd';
+import { useState } from 'react';
 
+const DeleteModal = ({ message, onCancel, onConfirm }) => {
+    const [visible, setVisible] = useState(true);
 
-const DeleteModal = ({ message, visible, onCancel, onConfirm }) => {
+    const handleClose = () => {
+        onCancel();
+        setVisible(false);
+    };
 
+    const handleConfirm = () => {
+        onConfirm(message.id);
+        setVisible(false);
+    };
 
     return (
-        <Modal
-            visible={visible}
-            title="Удалить сообщение"
-            onCancel={onCancel}
-            footer={[
-                <Button key="cancel" onClick={onCancel}>
-                    Отмена
-                </Button>,
-                <Button
-                    key="delete"
-                    type="primary"
-                    onClick={() => onConfirm(message.id)}
-                >
-                    Удалить
-                </Button>
-
-
-                ,
-            ]}
-        >
-            <p>Вы уверены, что хотите удалить это сообщение?</p>
-            <p>{message.senderName}: {message.text}</p>
-        </Modal>
+        message && (
+            <Modal
+                open={visible}
+                title="Удалить сообщение"
+                onCancel={handleClose}
+                footer={[
+                    <Button key="cancel" onClick={handleClose}>
+                        Отмена
+                    </Button>,
+                    <Button
+                        key="delete"
+                        type="primary"
+                        onClick={handleConfirm}
+                    >
+                        Удалить
+                    </Button>
+                ]}
+            >
+                <p>Вы уверены, что хотите удалить это сообщение?</p>
+                <p>{message.senderName}: {message.text}</p>
+            </Modal>
+        )
     );
-};
+}
 
-export default DeleteModal
+export default DeleteModal;
--- src/Components/FirebaseConfig.jsx
+++ src/Components/FirebaseConfig.jsx
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -13,7 +14,8 @@ const firebaseConfig = {
 };
 
 
-initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const database = getDatabase(firebaseApp);
 
-export const getFirebaseAuth = () => getAuth();
-export const getFirebaseDatabase = () => getDatabase();
+export { auth, database };
