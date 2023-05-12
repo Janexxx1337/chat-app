4b77865b15f33be410d02140cd6534185b511170
Your Name <yourgmail address>
2023-05-12 16:41:11 +0300

    add left swipe delete logic

--- src/Components/MessageContainer.jsx
+++ src/Components/MessageContainer.jsx
@@ -1,41 +1,57 @@
+import { useSwipeable } from 'react-swipeable';
 import {List} from "antd";
 import {DeleteOutlined} from "@ant-design/icons";
 import {isEmpty} from "lodash";
 import React from "react";
 
-const MessageContainer = (prop) => {
+const MessageItem = ({ msg, user, usersData, handleDelete }) => {
+    const handlers = useSwipeable({
+        onSwipedLeft: () => handleDelete(msg)
+    });
+
+    return (
+        <div {...handlers}>
+            <List.Item
+                key={msg.id}
+                actions={[
+                    user && msg.sender === user.uid && (
+                        <DeleteOutlined
+                            key="delete"
+                            onClick={() => handleDelete(msg)}
+                        />
+                    ),
+                ].filter((action) => !isEmpty(action))}
+            >
+                {msg.sender === user.uid ? "Вы: " : `${usersData[msg.sender]?.displayName || "пользователь"}: `}
+                {msg.text}
+            </List.Item>
+        </div>
+    );
+};
+
+const MessageContainer = (props) => {
 
     const handleDelete = (message) => {
-
-        if (prop.user && message.sender === prop.user.uid) {
-            prop.setSelectedMessageId(message.id);
+        if (props.user && message.sender === props.user.uid) {
+            props.setSelectedMessageId(message.id);
         }
     };
 
     return (
         <div className="messages-container">
             <List
-                dataSource={prop.messages.filter((msg) => !msg.receiver)}
+                dataSource={props.messages.filter((msg) => !msg.receiver)}
                 renderItem={(msg, index) => (
-                    <List.Item
-                        key={msg.id}
-                        actions={[
-                            prop.user && msg.sender === prop.user.uid && (
-                                <DeleteOutlined
-                                    key="delete"
-                                    onClick={() => handleDelete(msg)}
-                                />
-
-                            ),
-                        ].filter((action) => !isEmpty(action))}
-                    >
-                        {msg.sender === prop.user.uid ? "Вы: " : `${prop.usersData[msg.sender]?.displayName || "пользователь"}: `}
-                        {msg.text}
-                    </List.Item>
+                    <MessageItem
+                        msg={msg}
+                        user={props.user}
+                        usersData={props.usersData}
+                        handleDelete={handleDelete}
+                    />
                 )}
             />
         </div>
     )
 }
 
-export default MessageContainer
+export default MessageContainer;
