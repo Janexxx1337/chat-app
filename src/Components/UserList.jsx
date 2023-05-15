818e110f16433c681d35938572c277e8c3e14f9a
Your Name <yourgmail address>
2023-05-15 14:17:15 +0300

    add contacts searching & fix contact list width

--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { List, Badge, Avatar, Tooltip } from 'antd';
-import { UserOutlined, NotificationOutlined } from '@ant-design/icons';
+import { List, Badge, Avatar, Tooltip, Input } from 'antd';
+import { UserOutlined, NotificationOutlined, SearchOutlined } from '@ant-design/icons';
 import { ref, onValue } from 'firebase/database';
 import { database } from './FirebaseConfig';
 import { useNavigate } from "react-router-dom";
 
 const UserList = ({ setSelectedUser, setIsPrivateChat, user, setPrivateChatUser, isUserListVisible }) => {
     const [users, setUsers] = useState([]);
+    const [search, setSearch] = useState("");
     const [usersWithNotifications, setUsersWithNotifications] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [pageSize, setPageSize] = useState(5);
@@ -40,7 +41,13 @@ const UserList = ({ setSelectedUser, setIsPrivateChat, user, setPrivateChatUser,
         });
     }, []);
 
-    const filteredUsers = users.filter((u) => u.uid !== user?.uid);
+    const handleSearch = (event) => {
+        setSearch(event.target.value);
+    }
+
+    const filteredUsers = users
+        .filter((u) => u.uid !== user?.uid)
+        .filter((u) => u.displayName.toLowerCase().includes(search.toLowerCase()));
 
     const handleUserClick = (userId) => {
         setSelectedUser(userId);
@@ -62,6 +69,13 @@ const UserList = ({ setSelectedUser, setIsPrivateChat, user, setPrivateChatUser,
         isUserListVisible && (
             <div className={'user-list'} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <h3>Список пользователей:</h3>
+                <Input
+                    placeholder="Поиск по контактам..."
+                    prefix={<SearchOutlined />}
+                    value={search}
+                    onChange={handleSearch}
+                    style={{ marginBottom: '1rem' }}
+                />
                 <List
                     dataSource={paginatedUsers}
                     renderItem={(user, index) => (
